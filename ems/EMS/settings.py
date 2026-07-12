603eb6d755b9a59ff1f1b9c6c60197bfc7297eb4
ritika-adk <ashikariritka7@gmail.com>
2026-07-12 13:11:36 +0600

    update settings.py

--- ems/EMS/settings.py
+++ ems/EMS/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = config("DEBUG",cast=bool,defauly=False)
 
 ALLOWED_HOSTS = ['192.168.100.219', 'localhost', '127.0.0.1']
 
